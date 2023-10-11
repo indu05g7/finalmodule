75b6c221f4927197f5edcc77db13f9a962eb81db
indu05g7 <84284031+indu05g7@users.noreply.github.com>
2023-10-11 15:01:44 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/finalmodule/UserVerification.java
@@ -0,0 +1,15 @@
+package com.lbrce.finalmodule;
+
+import java.util.ResourceBundle;
+
+public class UserVerification {
+	public boolean check(String uname,String pswd) {
+		ResourceBundle rb= ResourceBundle.getBundle("config");
+		String username=rb.getString("username");
+		String password=rb.getString("password");
+		if(uname.equals(username)&&password.equals(password))
+			return true;
+		else 
+			return false;
+	}
+}
